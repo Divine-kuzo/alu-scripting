851c5bff9b46f2cba1540125bfa6d111b0937205
Divine-kuzo <d.kuzo@alustudent.com>
2025-07-19 10:48:36 -0700

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-""" top_ten.py """
 import requests
 
-
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """Prints the titles of the first 10 hot posts of a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            print(None)
+            return
+
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+
+        for post in posts:
+            print(post['data']['title'])
+
+    except Exception:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
